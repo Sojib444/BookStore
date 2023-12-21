4b5bc0f6732713bf5b8fe8de57dde66c4379069f
sojib <mdsojibhosen444@gmail.com>
2023-12-21 07:25:34 +0600

    Not COmpleted

--- src/test/Acme.BookStore.Application.Tests/Books/BookAppService_Tests.cs
+++ src/test/Acme.BookStore.Application.Tests/Books/BookAppService_Tests.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Shouldly;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Xunit;
 
-namespace Acme.BookStore.Books
+namespace Acme.BookStore.Books;
+
+
+public class BookAppService_Tests 
 {
-    internal class BookAppService_Tests
+    private readonly IBookAppService _bookAppService;
+
+    public BookAppService_Tests()
     {
+        
+    }
+
+    [Fact]
+    public async Task Should_Get_List_Of_Books()
+    {
+        //Act
+        var result = await _bookAppService.GetListAsync(
+            new PagedAndSortedResultRequestDto()
+        );
+
+        //Assert
+        result.TotalCount.ShouldBeGreaterThan(0);
+        result.Items.ShouldContain(b => b.Name == "1984");
     }
 }
