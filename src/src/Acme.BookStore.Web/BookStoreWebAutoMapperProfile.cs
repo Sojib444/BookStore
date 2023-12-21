b6f077b2420a8d937887b6c34ea18e67a3be6bcb
sojib <mdsojibhosen444@gmail.com>
2023-12-21 11:49:00 +0600

    Complete Author

--- src/src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ src/src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -1,5 +1,8 @@
-﻿using Acme.BookStore.Books;
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
+using Acme.BookStore.Web.Pages.Authors;
 using AutoMapper;
+using Volo.Abp.Identity.Web.Pages.Identity.Roles;
 
 namespace Acme.BookStore.Web;
 
@@ -10,5 +13,11 @@ public class BookStoreWebAutoMapperProfile : Profile
         //Define your AutoMapper configuration here for the Web project.
 
         CreateMap<BookDto, CreateUpdateBookDto>();
+
+        //Author
+        CreateMap<CreateModal.CreateAuthorViewModel,CreateAuthorDto>();
+        CreateMap<AuthorDto, Pages.Authors.EditModalModel.EditAuthorViewModel>();
+        CreateMap<Pages.Authors.EditModalModel.EditAuthorViewModel,
+                    UpdateAuthorDto>();
     }
 }
