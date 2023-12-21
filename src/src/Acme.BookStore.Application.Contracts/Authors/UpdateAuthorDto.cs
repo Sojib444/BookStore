d97f5b72f15221fa8f56515c9825b8243ecad424
sojib <mdsojibhosen444@gmail.com>
2023-12-21 11:08:18 +0600

    Complete Part-9 without Testing

---
+++ src/src/Acme.BookStore.Application.Contracts/Authors/UpdateAuthorDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.BookStore.Authors;
+
+public class UpdateAuthorDto
+{
+    [Required]
+    [StringLength(64)]
+    public string Name { get; set; }
+
+    [Required]
+    public DateTime BirthDate { get; set; }
+
+    public string ShortBio { get; set; }
+}
